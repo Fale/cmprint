885a8b2cfea065104191b6bd9a6736fbeb04d9e2
Fabio Alessandro Locati <fabiolocati@gmail.com>
2010-11-09 12:30:01 +0100

    mostrata massimizzata all'avvio

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+   // w.setFixedHeight(700);
+    w.showMaximized();
+   // w.show();
 
     return a.exec();
 }
