1075ec475b2434079ba9198d6c76e32293474ae0
Fabio Alessandro Locati <fabiolocati@gmail.com>
2011-01-13 23:06:01 +0100

    risolti problemi del foglio 4 e iniziato lavoro sul layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -325,6 +325,7 @@ QString MainWindow::creaHtml(int numero)
     QString foglio2;
     QString foglio3;
     QString foglio4;
+    QString foglio5;
 
     double prime[12];
     double successive[6];
@@ -407,7 +408,10 @@ foglio2 = "<div style=";
 
     //molto poco elegante farlo con gli spazi, calcolare la lunghezza del numero da sostituire alla X ed eventualmente aggiungere spazi per migliorare l'impaginazione
 
-    intestazione = " &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Prime X Copie &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; Successive X Copie<br>";
+    //intestazione = " &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Prime X Copie &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; Successive X Copie<br>";
+    //intestazione = "<table style=\"text-align: left; width: 500px; margin-left: auto; margin-right: 0px;\" border=\"1\" cellpadding=\"2\" cellspacing=\"2\"> <tbody> <tr> <td style=\"vertical-align: top; width: 240px; text-align: center;\">Prime X copie<br> </td> <td style=\"vertical-align: top; width: 240px; text-align: center;\">Successive X copie<br> </td> </tr> </tbody></table><br>";
+    intestazione = "<table align=\"right\"> <tbody><tr> <td width=\"150\">Prime X copie</td> <td width=\"150\">Successive X copie</td> </tr></tbody></table><br>";
+    intestazione.append("<table align=\"right\"> <tbody><tr> <td width=\"150\">X</td> <td width=\"150\">X</td> </tr></tbody></table><br>");
     intestazione.replace("X", campo.value(4).toString());
 
     foglio2 = intestazione;
@@ -548,7 +552,7 @@ foglio2 = "<div style=";
 
     foglio4.append("<br>Confezione:<br>");
 
-    if (!campo.value(87).toString().isEmpty())
+    if (campo.value(87).toDouble() != 0)
     {
         foglio4.append("Fustella:&nbsp;&nbsp;&nbsp;&nbsp; FUSTELLA<br>");
         foglio4.replace("FUSTELLA", campo.value(87).toString());
@@ -576,29 +580,29 @@ foglio2 = "<div style=";
     if (!campo.value(94).toString().isEmpty())
     {
         foglio4.append("Accoppiatura &nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SUCC<br>");
-        foglio4.replace("PRIME", campo.value(94).toDouble());
-        foglio4.replace("SUCC", campo.value(95).toDouble());
+        foglio4.replace("PRIME", campo.value(94).toString());
+        foglio4.replace("SUCC", campo.value(95).toString());
     }
 
     if (!campo.value(96).toString().isEmpty())
     {
         foglio4.append("Stampa a caldo &nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SUCC<br>");
-        foglio4.replace("PRIME", campo.value(96).toDouble());
-        foglio4.replace("SUCC", campo.value(97).toDouble());
+        foglio4.replace("PRIME", campo.value(96).toString());
+        foglio4.replace("SUCC", campo.value(97).toString());
     }
 
     if (!campo.value(98).toString().isEmpty())
     {
         foglio4.append("Cliche' &nbsp;&nbsp; PRIME<br>");
-        foglio4.replace("PRIME", campo.value(98).toDouble());
+        foglio4.replace("PRIME", campo.value(98).toString());
     }
 
 
     if (ui->label_foglio4_piegacopie_primencopie->text().toDouble() != 0)
     {
         foglio4.append("<br>Piega&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>");
-        foglio4.replace("COPIE", campo.value(99).toDouble());
-        foglio4.replace("EURO", campo.value(100).toDouble());
+        foglio4.replace("COPIE", campo.value(99).toString());
+        foglio4.replace("EURO", campo.value(100).toString());
         foglio4.replace("PRIME",  ui->label_foglio4_piegacopie_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_piegacopie_successivencopie->text());
     }
@@ -606,8 +610,8 @@ foglio2 = "<div style=";
     if (ui->label_foglio4_tagliocopie_primencopie->text().toDouble() != 0)
     {
         foglio4.append("<brTaglio&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>");
-        foglio4.replace("COPIE", campo.value(101).toDouble());
-        foglio4.replace("EURO", campo.value(102).toDouble());
+        foglio4.replace("COPIE", campo.value(101).toString());
+        foglio4.replace("EURO", campo.value(102).toString());
         foglio4.replace("PRIME", ui->label_foglio4_tagliocopie_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_tagliocopie_successivencopie->text());
     }
@@ -615,18 +619,18 @@ foglio2 = "<div style=";
     if (ui->label_foglio4_puntometallico_primencopie->text().toDouble() != 0)
     {
         foglio4.append("<br>Punto metallico&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME<br>Avviamento&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AVVIAMENTO<br>");
-        foglio4.replace("COPIE", campo.value(103).toDouble());
-        foglio4.replace("EURO", campo.value(104).toDouble());
-        foglio4.replace("AVVIAMENTO", campo.value(105).toDouble());
+        foglio4.replace("COPIE", campo.value(103).toString());
+        foglio4.replace("EURO", campo.value(104).toString());
+        foglio4.replace("AVVIAMENTO", campo.value(105).toString());
         foglio4.replace("PRIME", ui->label_foglio4_puntometallico_primencopie->text());
     }
 
     if (ui->label_foglio4_brosurafresata_primencopie->text().toDouble() != 0)
     {
         foglio4.append("Brosura fresata&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>Avviamento&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AVVIAMENTO<br>");
-        foglio4.replace("COPIE", campo.value(106).toDouble());
-        foglio4.replace("EURO", campo.value(107).toDouble());
-        foglio4.replace("AVVIAMENTO", campo.value(108).toDouble());
+        foglio4.replace("COPIE", campo.value(106).toString());
+        foglio4.replace("EURO", campo.value(107).toString());
+        foglio4.replace("AVVIAMENTO", campo.value(108).toString());
         foglio4.replace("PRIME", ui->label_foglio4_brosurafresata_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_brosurafresata_successivencopie->text());
     }
@@ -634,9 +638,9 @@ foglio2 = "<div style=";
     if (ui->label_foglio4_filo_primencopie->text().toDouble() != 0)
     {
         foglio4.append("Brosura filo refe&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>Avviamento&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AVVIAMENTO<br>");
-        foglio4.replace("COPIE", campo.value(109).toDouble());
-        foglio4.replace("EURO", campo.value(110).toDouble());
-        foglio4.replace("AVVIAMENTO", campo.value(111).toDouble());
+        foglio4.replace("COPIE", campo.value(109).toString());
+        foglio4.replace("EURO", campo.value(110).toString());
+        foglio4.replace("AVVIAMENTO", campo.value(111).toString());
         foglio4.replace("PRIME", ui->label_foglio4_filo_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_filo_successivencopie->text());
     }
@@ -644,9 +648,9 @@ foglio2 = "<div style=";
     if (ui->label_foglio4_cartonato_primencopie->text().toDouble() != 0)
     {
         foglio4.append("Cartonato&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>Avviamento&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AVVIAMENTO<br>");
-        foglio4.replace("COPIE", campo.value(112).toDouble());
-        foglio4.replace("EURO", campo.value(113).toDouble());
-        foglio4.replace("AVVIAMENTO", campo.value(114).toDouble());
+        foglio4.replace("COPIE", campo.value(112).toString());
+        foglio4.replace("EURO", campo.value(113).toString());
+        foglio4.replace("AVVIAMENTO", campo.value(114).toString());
         foglio4.replace("PRIME", ui->label_foglio4_cartonato_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_cartonato_successivencopie->text());
     }
@@ -655,8 +659,8 @@ foglio2 = "<div style=";
     if (ui->label_foglio4_spiralatura_primencopie->text().toDouble() != 0)
     {
         foglio4.append("Spiralatura&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>");
-        foglio4.replace("COPIE", campo.value(115).toDouble());
-        foglio4.replace("EURO", campo.value(116).toDouble());
+        foglio4.replace("COPIE", campo.value(115).toString());
+        foglio4.replace("EURO", campo.value(116).toString());
         foglio4.replace("PRIME", ui->label_foglio4_spiralatura_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_spiralatura_successivencopie->text());
     }
@@ -664,8 +668,8 @@ foglio2 = "<div style=";
     if (ui->label_foglio4_pacchi_primencopie->text().toDouble() != 0)
     {
         foglio4.append("Pacchi politenati&nbsp; &nbsp;&nbsp;&nbsp; copie: COPIE xEuro:EURO&nbsp;&nbsp;&nbsp;&nbsp;PRIME&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SUCC<br>");
-        foglio4.replace("COPIE", campo.value(117).toDouble());
-        foglio4.replace("EURO", campo.value(118).toDouble());
+        foglio4.replace("COPIE", campo.value(117).toString());
+        foglio4.replace("EURO", campo.value(118).toString());
         foglio4.replace("PRIME", ui->label_foglio4_pacchi_primencopie->text());
         foglio4.replace("SUCC", ui->label_foglio4_pacchi_successivencopie->text());
     }
@@ -694,8 +698,8 @@ foglio2 = "<div style=";
     foglio4.replace("TOTPRIME", ui->label_foglio4_totale_primencopie->text());
     foglio4.replace("TOTSUCC", ui->label_foglio4_totale_successivencopie->text());
 
-
-
+    //foglio5
+    foglio5 = intestazione;
 
 
 
@@ -703,6 +707,7 @@ foglio2 = "<div style=";
     completo.append(foglio2);
     completo.append(foglio3);
     completo.append(foglio4);
+    completo.append(foglio5);
     completo.append("</body></html>");
     return completo;
 }
