ab92c41fdd6dee885afffd66a2ffae62f9b650af
Fabio Alessandro Locati <fabiolocati@gmail.com>
2011-07-21 15:18:34 +0200

    remove commented code

--- main.cpp
+++ main.cpp
@@ -5,11 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-   //w.resize(800,600);
-   // w.setMinimumHeight(700);
-    //w.show();
-    //w.setMaximumHeight(600);
-    //w.setMaximumWidth(1024);
     w.showMaximized();
     return a.exec();
 }
